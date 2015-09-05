44937dda4c3c6c3db65f5dd73e1f08da3a71db00
Stanislau Kavaliou <kavalioustanislau@gmail.com>
2015-09-05 18:07:42 +0400

    start doing auth

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from Tasky import settings
+
+
+class preRegistration(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)

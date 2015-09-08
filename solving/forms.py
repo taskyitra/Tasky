556a1846c9189f1312e23de12f7aba098178bd01
Stanislau Kavaliou <kavalioustanislau@gmail.com>
2015-09-08 14:07:23 +0400

    delete solving app(зря добавил)

--- solving/forms.py
+++
@@ -1,5 +0,0 @@
-from django.forms import Form
-from django import forms
-
-class TaskSolvingForm(Form):
-    answer = forms.CharField(max_length=50, widget=forms.TextInput)
--- solving/models.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-from task.models import Task
-
-

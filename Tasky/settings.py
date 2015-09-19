9ae8c325cebfaa84688fbaa7981af8b7b1102e89
Stanislau Kavaliou <kavalioustanislau@gmail.com>
2015-09-19 21:48:51 +0400

    some changes before pushing on heroku

--- Tasky/settings.py
+++ Tasky/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'Tasky/static'),
 )
 
 AUTHENTICATION_BACKENDS = (
